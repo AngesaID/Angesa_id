c3ba9ce6721cbea3fff845efec3b1fb63026239b
austhopia <fathurrahman.syarief-2020@stmm.unair.ac.id>
2021-12-30 01:24:04 +0700

    integrated model to flask

---
+++ angesa/src/models.py
@@ -0,0 +1,78 @@
+from gensim.models import fasttext
+from gensim.models.fasttext import load_facebook_model
+import re
+import pickle
+import itertools
+from collections import Counter
+import json
+
+
+with open('src/pickle/models.pkl', 'rb') as file:
+    vocab = pickle.load(file)
+    
+words = list(vocab)
+    
+w_rank = {}
+for iter,word in enumerate(words):
+    w_rank[word] = iter
+        
+WORDS = w_rank
+
+dict(itertools.islice(WORDS.items(), 10))
+
+
+def words(text): return re.findall(r'\w+', text.lower())
+
+
+def P(word): 
+    "Probability of `word`."
+    # use inverse of rank as proxy
+    # returns 0 if the word isn't in the dictionary
+    return - WORDS.get(word, 0)
+
+def correction(word): 
+    "Most probable spelling correction for word."
+    return max(candidates(word), key=P)
+
+def candidates(word): 
+    "Generate possible spelling corrections for word."
+    return (known([word]) or known(edits1(word)) or known(edits2(word)) or [word])
+
+def known(words): 
+    "The subset of `words` that appear in the dictionary of WORDS."
+    return set(w for w in words if w in WORDS)
+
+def edits1(word):
+    "All edits that are one edit away from `word`."
+    letters    = 'abcdefghijklmnopqrstuvwxyz'
+    splits     = [(word[:i], word[i:])    for i in range(len(word) + 1)]
+    deletes    = [L + R[1:]               for L, R in splits if R]
+    transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R)>1]
+    replaces   = [L + c + R[1:]           for L, R in splits if R for c in letters]
+    inserts    = [L + c + R               for L, R in splits for c in letters]
+    return set(deletes + transposes + replaces + inserts)
+
+def edits2(word): 
+    "All edits that are two edits away from `word`."
+    return (e2 for e1 in edits1(word) for e2 in edits1(e1))
+
+
+with open("src/source/slang.txt") as f:
+    slangS = json.loads(f.read())
+    
+    
+def typechecker(T):
+    Texts = re.findall(r"[\w']+|[.,!?;]",T)
+    
+    _spelling = []
+    for text in Texts:
+        _spelling.append(correction(text))
+    
+    for index,text in enumerate(_spelling):
+        if text in slangS.keys():
+            _spelling[index] = slangS[text]
+    
+    _text = ' '.join(_spelling)
+    _text = re.sub(r' ([^A-Za-z0-9])', r'\1', _text)
+    
+    return _text
--- angesa/src/routes.py
+++ angesa/src/routes.py
@@ -1,9 +1,24 @@
-from flask import render_template, request
+from flask import render_template, request, redirect
+from flask.helpers import url_for
 from src import app
+from src.models import typechecker
 
 
+
+InputText = []
 @app.route('/')
 @app.route('/home')
-
 def home_page():
     return render_template('index.html')
+
+@app.route('/demo', methods=["POST", "GET"])
+def demo_page():
+    if request.method == "POST":
+        text = request.form.get("inputtext")
+        
+        result = typechecker(text)
+        
+        return render_template('demo.html', initialtext=text ,finaltext=result)
+    
+    else:
+        return render_template('demo.html')
